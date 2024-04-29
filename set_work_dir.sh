a518cbac746eac19f01f440ebf2d585c286a3cfb
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2024-04-29 15:59:43 +0330

    fix: .end file doesn't get updated

--- set_work_dir.sh
+++ set_work_dir.sh
@@ -4,10 +4,10 @@
 # set -x
 
 # Get the path of the current script
-script_path="${BASH_SOURCE[0]}"
+SCRIPT_PATH="${BASH_SOURCE[0]}"
 
 # Convert Windows-style line endings to Unix-style line endings
-sed -i '' 's/\r$//' "$script_path"
+sed -i '' 's/\r$//' "$SCRIPT_PATH"
 
 # Get the current directory name using ${pwd##*/}
 CURRENT_DIR_NAME="${PWD##*/}"
@@ -18,20 +18,11 @@ CURRENT_DIR_PATH=$(pwd)
 OS_USER=$(whoami)
 OS_UID=$(id -u)
 
-# Define an associative array to map variable names to their values
-declare -A VAR_VALUES=(
-  ["COMPOSE_PROJECT_NAME"]=$CURRENT_DIR_NAME
-  ["PROJECT_FOLDER_NAME"]=$CURRENT_DIR_NAME
-  ["PROJECT_VOLUME"]="./$CURRENT_DIR_NAME:/opt/project"
-  ["WORKING_DIR"]="/opt/project"
-  ["OS_USER"]=$OS_USER
-  ["UID"]=$OS_UID
-)
-
 # Define the path to your .env file
 DOCKER_ENV_FILE="$CURRENT_DIR_PATH/.env.docker"
-# move up 1 directory from current directory and set the .env file
-ENV_FILE="$CURRENT_DIR_PATH/../.env"
+PARENT_DIR_PATH=$(dirname "$CURRENT_DIR_PATH")
+# Define the path to the .env file in the parent directory
+ENV_FILE="$PARENT_DIR_PATH/.env"
 
 # Create the .env file if it does not exist
 if [ ! -e "$DOCKER_ENV_FILE" ]; then
@@ -41,6 +32,32 @@ if [ ! -e "$DOCKER_ENV_FILE" ]; then
   else
     echo "Error: Failed to create $DOCKER_ENV_FILE"
   fi
+fi
+
+# Define an associative array to map variable names to their values
+declare -A DYNAMIC_ENV_VARS=(
+  ["COMPOSE_PROJECT_NAME"]=$CURRENT_DIR_NAME
+  ["PROJECT_FOLDER_NAME"]=$CURRENT_DIR_NAME
+  ["PROJECT_VOLUME"]="./$CURRENT_DIR_NAME:/opt/project"
+  ["WORKING_DIR"]="/opt/project"
+  ["OS_USER"]=$OS_USER
+  ["UID"]=$OS_UID
+)
+DEFAULT_PHP_SERVICE="php81"
+# Check if the PHP_SERVICE variable is set in the .docker.env file
+if grep -q "^PHP_SERVICE=" "$DOCKER_ENV_FILE"; then
+  # If its value is empty, it will be set to php and we tell the user
+  PHP_SERVICE=$(grep "^PHP_SERVICE=" "$DOCKER_ENV_FILE" | cut -d '=' -f2)
+  if [ -z "$PHP_SERVICE" ]; then
+    echo "PHP_SERVICE is empty in $DOCKER_ENV_FILE, setting it to $DEFAULT_PHP_SERVICE"
+    DYNAMIC_ENV_VARS["PHP_SERVICE"]=$DEFAULT_PHP_SERVICE
+  else
+  echo "PHP_SERVICE is set in $DOCKER_ENV_FILE, setting it to $PHP_SERVICE"
+    DYNAMIC_ENV_VARS["PHP_SERVICE"]=$PHP_SERVICE
+  fi
+else
+  echo "PHP_SERVICE is not set in $DOCKER_ENV_FILE, setting it to $DEFAULT_PHP_SERVICE"
+  DYNAMIC_ENV_VARS["PHP_SERVICE"]=$DEFAULT_PHP_SERVICE
 fi
 
 # Check if the .env file is readable and writable
@@ -54,16 +71,20 @@ fi
 # Check if the .env file exists
 if [ -e "$DOCKER_ENV_FILE" ]; then
   # Loop through the associative array and replace variable values
-  for VAR_NAME in "${!VAR_VALUES[@]}"; do
-    VAR_VALUE="${VAR_VALUES[$VAR_NAME]}"
+  for VAR_NAME in "${!DYNAMIC_ENV_VARS[@]}"; do
+    VAR_VALUE="${DYNAMIC_ENV_VARS[$VAR_NAME]}"
     OLD_VALUE=$(grep "^$VAR_NAME=" "$DOCKER_ENV_FILE" | cut -d '=' -f2)
     # Replace the value if the key exists
     sed -i '' "s^$VAR_NAME=.*^$VAR_NAME=$VAR_VALUE^" "$DOCKER_ENV_FILE"
     sed -i '' "s^$VAR_NAME=.*^$VAR_NAME=$VAR_VALUE^" "$ENV_FILE"
 
-    # Add the key if it does not exist
+    # Add the key if it does not exist in the .env.docker file
     if ! grep -q "^$VAR_NAME=" "$DOCKER_ENV_FILE"; then
       echo "$VAR_NAME=$VAR_VALUE" >> "$DOCKER_ENV_FILE"
+    fi
+    # Add the key if it does not exist in the .env file
+    if ! grep -q "^$VAR_NAME=" "$ENV_FILE"; then
+      echo "$VAR_NAME=$VAR_VALUE" >> "$ENV_FILE"
     fi
 
     # Print information about the updated variable only if the value has changed
